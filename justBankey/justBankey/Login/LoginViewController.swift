f434414652acc057f4bd063b66c9b412665ac009
Kirill Ki <kirillkalapov@gmail.com>
2022-03-08 19:27:22 +0100

    Add login screen

--- justBankey/justBankey/Login/LoginViewController.swift
+++ justBankey/justBankey/Login/LoginViewController.swift
@@ -12,6 +12,14 @@ class LoginViewController: UIViewController {
     let loginView = LoginView()
     let signButton = UIButton(type: .system)
     let errorMessageLabel = UILabel()
+    let logoLabel = UILabel()
+    
+    var username: String? {
+        return loginView.userNameTextField.text
+    }
+    var password: String? {
+        return loginView.passwordTextField.text
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,18 +47,30 @@ extension LoginViewController {
         errorMessageLabel.textAlignment = .center
         errorMessageLabel.textColor = .systemRed
         errorMessageLabel.numberOfLines = 0
-        errorMessageLabel.text = "Error failure"
+     //   errorMessageLabel.text = "Error failure"
         errorMessageLabel.isHidden = true
+        
+        logoLabel.translatesAutoresizingMaskIntoConstraints = false
+        logoLabel.text = "justBankey"
+        logoLabel.font = .boldSystemFont(ofSize: 30)
+        logoLabel.textColor = .systemGray
+        logoLabel.textAlignment = .center
         
         
     }
     
     private  func layout() {
+       
         view.addSubview(loginView)
         view.addSubview(signButton)
         view.addSubview(errorMessageLabel)
+        view.addSubview(logoLabel)
         
         NSLayoutConstraint.activate([
+            
+           
+            
+            
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 2),
@@ -61,7 +81,11 @@ extension LoginViewController {
             
             errorMessageLabel.topAnchor.constraint(equalToSystemSpacingBelow: signButton.bottomAnchor, multiplier:  2),
             errorMessageLabel.leadingAnchor.constraint(equalTo: signButton.leadingAnchor),
-            errorMessageLabel.trailingAnchor.constraint(equalTo: signButton.trailingAnchor)
+            errorMessageLabel.trailingAnchor.constraint(equalTo: signButton.trailingAnchor),
+            
+            logoLabel.bottomAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 30),
+            logoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
             
         ])
         
@@ -71,6 +95,31 @@ extension LoginViewController {
 
 extension LoginViewController {
     @objc func signInTapped(sender: UIButton) {
+        errorMessageLabel.isHidden = true
+        login()
         
+    }
+    
+    private func login() {
+        guard let username = username, let password = password else {
+            assertionFailure("Username / password should never be nil")
+            return
+        }
+        
+        if username.isEmpty || password.isEmpty {
+            configureView(withMessage: "Username / password cannot be blank")
+            return
+        }
+        
+        if username == "Kirill" && password == "12345" {
+            signButton.configuration?.showsActivityIndicator = true
+        } else {
+            configureView(withMessage: "Incorrect username or password")
+        }
+    }
+    
+    private func configureView(withMessage message: String) {
+        errorMessageLabel.isHidden = false
+        errorMessageLabel.text = message
     }
 }
