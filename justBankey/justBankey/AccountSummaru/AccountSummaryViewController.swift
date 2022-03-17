46321faa563017da9836bccbf3a1de62345e34e1
Kirill Ki <kirillkalapov@gmail.com>
2022-03-17 21:18:21 +0100

    Handle network errors with alert

--- justBankey/justBankey/AccountSummaru/AccountSummaryViewController.swift
+++ justBankey/justBankey/AccountSummaru/AccountSummaryViewController.swift
@@ -197,7 +197,7 @@ extension AccountSummaryViewController {
           //      self.configureTableHeaderView(with: profile)
              //   self.tableView.reloadData()
             case .failure(let error):
-                print(error.localizedDescription)
+                self.displayError(error)
             }
             group.leave()
         }
@@ -211,6 +211,7 @@ extension AccountSummaryViewController {
               //  self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
+                self.displayError(error)
             }
             group.leave()
         }
@@ -241,6 +242,32 @@ extension AccountSummaryViewController {
                                          balance: $0.amount)
         }
     }
+    
+    private func displayError(_ error: NetworkError) {
+        print(error.localizedDescription)
+        let title: String
+        let message: String
+        switch error {
+        case .serverError:
+            title = "Server Error"
+            message = "Check your internet connection"
+        case .decodingError:
+            title = "Decoding Error"
+            message = "We could not process your request"
+        }
+        self.showErrorAlert(title: title, message: message)
+    }
+    
+    private func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 //MARK: - Actions
